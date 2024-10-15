23f1af9743d5718faf46ef76f6f5e47fd9832f60
AnthonyBoschat <AnthonyBoschat.dev@hotmail.com>
2024-10-15 14:51:51 +0200

    initial commit

---
+++ Pages/openMenu.js
@@ -0,0 +1,34 @@
+const menuButton = document.querySelector(".menu-button")
+const closeNavigationButton = document.querySelector(".close-phone-navigation-button")
+const navigationContainer = document.querySelector(".phone-navigation-container")
+
+const handleOpenPhoneNavigationMenu = () => {
+    if(navigationContainer){
+        // Si le menu de navigation sur téléphone est fermer
+        if(navigationContainer.classList.contains("closed")){
+            // On retire la classe closed
+            navigationContainer.classList.remove("closed")
+            // On ajoute la classe d'animation d'apparition
+            navigationContainer.classList.add("animationClass_phone-navigation-apparition")
+        }
+    }
+}
+
+const handleClosePhoneNavigationMenu = () => {
+    if(navigationContainer){
+        // Si le menu de navigation sur téléphone est ouvert
+        if(!navigationContainer.classList.contains("closed")){
+            // On ajoute la classe d'animation de fermeture
+            navigationContainer.classList.add("animationClass_phone-navigation-disparition")
+            // A la fin de cette animation, on lui remet closed, et on lui retire toutes ses classes d'animations
+            setTimeout(() => {
+                navigationContainer.classList.add("closed")
+                navigationContainer.classList.remove("animationClass_phone-navigation-disparition")
+                navigationContainer.classList.remove("animationClass_phone-navigation-apparition")
+            }, 300);
+        }
+    }
+}
+
+menuButton.addEventListener("click", handleOpenPhoneNavigationMenu, true)
+closeNavigationButton.addEventListener("click", handleClosePhoneNavigationMenu, true)
